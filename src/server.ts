9b7a18e694d826ee701e12f5f8a82652ef4fb693
brunoeduardodev <brunoeduardo.dev@gmail.com>
2024-09-25 15:40:46 -0300

    initial commit

---
+++ src/server.ts
@@ -0,0 +1,46 @@
+import express from "express";
+import pino from "pino";
+
+const run = async () => {
+  const logger = pino();
+  const app = express();
+
+  app.use(express.json());
+
+  app.get("/", (req, res) => {
+    logger.info("Root request received");
+    res.json({ message: "Hello World!" });
+  });
+
+  app.get("/ping", (req, res) => {
+    logger.info("GET request received");
+    res.json({ message: "pong" });
+  });
+
+  app.post("/ping", (req, res) => {
+    logger.info("POST request received");
+    res.json({ message: "pong", body: req.body });
+  });
+
+  app.put("/ping", (req, res) => {
+    logger.info("PUT request received");
+    res.json({ message: "pong", body: req.body });
+  });
+
+  app.delete("/ping", (req, res) => {
+    logger.info("DELETE request received");
+    res.json({ message: "pong", method: "DELETE" });
+  });
+
+  app.listen(3000, () => {
+    logger.info("Server started on port 3000");
+
+    let iteration = 0;
+    setInterval(() => {
+      logger.info("Cron example running", { iteration });
+      iteration++;
+    }, 5000);
+  });
+};
+
+run();
